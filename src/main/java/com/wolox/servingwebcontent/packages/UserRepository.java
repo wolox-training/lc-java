745e472c70c9bec0490abfc08d4841254622dad7
Leon Calermo <lcalermo@MacBook-Pro-de-wolox-109.local>
2020-03-16 17:02:39 -0300

    added findByUsername

--- src/main/java/com/wolox/servingwebcontent/packages/UserRepository.java
+++ src/main/java/com/wolox/servingwebcontent/packages/UserRepository.java
@@ -1,8 +1,9 @@
 package com.wolox.servingwebcontent.packages;
 
 import com.wolox.servingwebcontent.models.User;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+    Optional<User> findFirstByUsername(String username);
 }
