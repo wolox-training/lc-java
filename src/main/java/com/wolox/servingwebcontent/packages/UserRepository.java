819db2da6c01a2dc19b485e4ab4d5e137cf9e063
Leon Calermo <lcalermo@MacBook-Pro-de-wolox-109.local>
2020-03-25 16:27:04 -0300

    changed user to users

--- src/main/java/com/wolox/servingwebcontent/packages/UserRepository.java
+++ src/main/java/com/wolox/servingwebcontent/packages/UserRepository.java
@@ -1,8 +1,8 @@
 package com.wolox.servingwebcontent.packages;
 
-import com.wolox.servingwebcontent.models.User;
+import com.wolox.servingwebcontent.models.Users;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<Users, Long> {
 
 }
